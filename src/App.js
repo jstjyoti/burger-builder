dc0b33bd2183fc3877ec60dc52a4b89fb4d6f00f
Jyoti Mishra <jyotimishra29091997@gmail.com>
2020-04-23 21:25:00 +0530

    Improving app and associating Burger Builder into

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import Layout from './components/Layout/Layout'
+import BurgerBuilder from './container/BurgerBuilder/BurgerBuilder'
 
 function App() {
   return (
     <div>
       <Layout>
-        <p>Testing Burger Buider</p>
+        <BurgerBuilder></BurgerBuilder>
       </Layout>
     </div>
   );
