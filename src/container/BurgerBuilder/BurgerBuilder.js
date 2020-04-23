d2ede6f08257613df8b2b6d115d7eaf563c6f261
Jyoti Mishra <jyotimishra29091997@gmail.com>
2020-04-23 21:07:12 +0530

    Basic BurgerBuilder Component Template

---
+++ src/container/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+import Aux from '../../hoc/Aux'
+
+class BurgerBuilder extends Component {
+  render(){
+    return <Aux>
+        <div>Burger Graphics</div>
+        <div>Build Components</div>
+    </Aux>
+  }
+}
+
+export default BurgerBuilder;
