1ba25f335fe1419568a06e647f46f1468f4c85db
Jyoti Mishra <jyotimishra29091997@gmail.com>
2020-04-25 17:54:41 +0530

    added life Cycle method in Modal for increasing performance

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,22 +1,24 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Button from '../../UI/Button/Button';
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients).map(el=> {
-    return (<li key={el}>
-          <span style={{textTransform: 'capitalize'}}>{el}</span>
-          : {props.ingredients[el]}
-        </li>);
-  })
-  return <>
-    <h3>Your Order</h3>
-    <p>Delicious Burger with following ingredients</p>
-    <ul>{ingredientSummary}</ul>
-    <p>Amount Payable: {props.price.toFixed(2)}</p>
-    <p>Continue To Checkout?</p>
-    <Button btnType='Danger' click={props.cancel}>Cancel</Button>
-    <Button btnType='Success' click={props.continue}>Continue</Button>
+class OrderSummary extends Component{
 
-  </>
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map(el=> {
+      return (<li key={el}>
+            <span style={{textTransform: 'capitalize'}}>{el}</span>
+            : {this.props.ingredients[el]}
+          </li>);
+    })
+    return <>
+      <h3>Your Order</h3>
+      <p>Delicious Burger with following ingredients</p>
+      <ul>{ingredientSummary}</ul>
+      <p>Amount Payable: {this.props.price.toFixed(2)}</p>
+      <p>Continue To Checkout?</p>
+      <Button btnType='Danger' click={this.props.cancel}>Cancel</Button>
+      <Button btnType='Success' click={this.props.continue}>Continue</Button>
+
+    </>
+  }
 }
-
-export default orderSummary;
+export default OrderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,25 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import Aux from '../../../hoc/Aux';
 import classes from './Modal.css';
 import BackDrop from '../BackDrop/BackDrop';
 
-const modal = (props) => {
-  let show = props.show ? 'translateY(0)' : 'translateY(-100vh)'
-  let opacity= props.show ? '1' : '0'
-  let style = {
-    transform: show,
-    opacity: opacity
+class Modal extends Component
+{ 
+  shouldComponentUpdate(nextProps, nextState){
+    return nextProps.show !== this.props.show;
   }
-  return <Aux>
-    <BackDrop show={props.show} clicked={props.modalClosed}></BackDrop>
-    <div className={classes.Modal}
-      style={style}>
-      {props.children}
-    </div>
-  </Aux>
+  render() {
+    let show = this.props.show ? 'translateY(0)' : 'translateY(-100vh)'
+    let opacity= this.props.show ? '1' : '0'
+    let style = {
+      transform: show,
+      opacity: opacity
+    }
+    return <Aux>
+      <BackDrop show={this.props.show} clicked={this.props.modalClosed}></BackDrop>
+      <div className={classes.Modal}
+        style={style}>
+        {this.props.children}
+      </div>
+    </Aux>
+  }
+
 }
   
 
 
-export default modal;
+export default Modal;
