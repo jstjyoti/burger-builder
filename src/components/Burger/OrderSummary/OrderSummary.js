c10a554f8e6835c594f2f8983897004bd77f66da
Jyoti Mishra <jyotimishra29091997@gmail.com>
2020-04-25 14:49:38 +0530

    added orderSummary for modal

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import Button from '../../UI/Button/Button';
 const orderSummary = (props) => {
   const ingredientSummary = Object.keys(props.ingredients).map(el=> {
     return (<li key={el}>
@@ -11,7 +11,10 @@ const orderSummary = (props) => {
     <h3>Your Order</h3>
     <p>Delicious Burger with following ingredients</p>
     <ul>{ingredientSummary}</ul>
+    <p>Amount Payable: {props.price.toFixed(2)}</p>
     <p>Continue To Checkout?</p>
+    <Button btnType='Danger' click={props.cancel}>Cancel</Button>
+    <Button btnType='Success' click={props.continue}>Continue</Button>
 
   </>
 }
