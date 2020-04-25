b17aa89d874ff03d4b5022be58c12503cccd1645
Jyoti Mishra <jyotimishra29091997@gmail.com>
2020-04-25 14:51:46 +0530

    added Modal Ccoponent

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -4,17 +4,22 @@ import Aux from '../../../hoc/Aux';
 import classes from './Modal.css';
 import BackDrop from '../BackDrop/BackDrop';
 
-const modal = (props) => (
-  <Aux>
+const modal = (props) => {
+  let show = props.show ? 'translateY(0)' : 'translateY(-100vh)'
+  let opacity= props.show ? '1' : '0'
+  let style = {
+    transform: show,
+    opacity: opacity
+  }
+  return <Aux>
     <BackDrop show={props.show} clicked={props.modalClosed}></BackDrop>
     <div className={classes.Modal}
-    style={{
-      tranform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-      opacity: props.show ? '1' : '0'
-    }}>
+      style={style}>
       {props.children}
     </div>
   </Aux>
-  );
+}
+  
+
 
 export default modal;
