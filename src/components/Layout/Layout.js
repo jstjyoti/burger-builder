dfe6a960d9c86bbfd54474a1d3d8db9e3818ead5
Jyoti Mishra <jyotimishra29091997@gmail.com>
2020-04-25 14:55:25 +0530

    Updated Layout with toolbar and sideDrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../../hoc/Aux';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar'; 
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer' ;
 import classes from './Layout.css'
-const layout = (props) => (
-  <Aux>
-      <div> Toolbar, SideDrawer, Backdrop </div>
+
+ class Layout extends Component{
+  state = {
+    showSideDrawer: false
+  }
+  sideDrawerHandler = () =>{
+    
+    this.setState((prevState) => { return {showSideDrawer : !prevState.showSideDrawer}});
+  }
+  sideDrawerCloseHandler = () =>{
+    this.setState({showSideDrawer : false})
+  }
+  render (){
+    return <Aux>
+      <Toolbar toggledrawer={this.sideDrawerHandler}></Toolbar>
+      <SideDrawer open={this.state.showSideDrawer} close={this.sideDrawerCloseHandler}></SideDrawer>
       <main className={classes.Content}>
-        {props.children}
+        {this.props.children}
       </main>
   </Aux>
-  );
+  }
+ } 
    
-export default layout;
+export default Layout;
