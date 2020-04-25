342cefd2c0cd0429167f9f69bd64385afc6a5540
Jyoti Mishra <jyotimishra29091997@gmail.com>
2020-04-25 14:53:31 +0530

    added Button Component to be used by NavigationItems in Toolbar

---
+++ src/components/UI/Button/Button.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import classes from './Button.css';
+
+const button = (props) => (
+  <button className={[classes.Button, classes[props.btnType]].join(' ')} 
+  onClick={props.click}>{props.children}</button>
+);
+
+export default button;
