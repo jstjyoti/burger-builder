eb9a1164e058866cbb78b46482f9924a39434b5f
Jyoti Mishra <jyotimishra29091997@gmail.com>
2020-04-29 13:29:06 +0530

    updated checking for shouldComponentUpdate

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -7,7 +7,7 @@ import BackDrop from '../BackDrop/BackDrop';
 class Modal extends Component
 { 
   shouldComponentUpdate(nextProps, nextState){
-    return nextProps.show !== this.props.show;
+    return nextProps.show !== this.props.show || nextProps.children !== this.props.children;
   }
   render() {
     let show = this.props.show ? 'translateY(0)' : 'translateY(-100vh)'
