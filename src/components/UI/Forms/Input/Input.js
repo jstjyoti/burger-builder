2a97ded00c888cede5eab0bb2e3151f6e6c9528c
Jyoti Mishra <jyotimishra29091997@gmail.com>
2020-05-09 16:30:00 +0530

    using invalid style

--- src/components/UI/Forms/Input/Input.js
+++ src/components/UI/Forms/Input/Input.js
@@ -3,25 +3,29 @@ import classes from './Input.css';
 
 const input = (props) => {
   let inputEle = null;
+  let inputClasses = [classes.InputElement];
+  if(props.invalid && props.shouldValidate){
+    inputClasses.push(classes.Invalid);
+  }
   switch(props.elementType) {
     case('input'): 
       inputEle = <input 
-        className={classes.InputElement} 
+        className={inputClasses.join(' ')} 
         {...props.elementConfig} 
         value={props.value}
         onChange={props.change}
       />
       break;
     case('textarea'):
-      inputEle = <textarea className={classes.InputElement} {...props.elementConfig} value={props.value} onChange={props.change}/>
+      inputEle = <textarea className={inputClasses.join(' ')} {...props.elementConfig} value={props.value} onChange={props.change}/>
       break;
     case('select'):
-      inputEle = <select className={classes.InputElement} {...props.elementConfig} value={props.value} onChange={props.change}>
+      inputEle = <select className={inputClasses.join(' ')} {...props.elementConfig} value={props.value} onChange={props.change}>
         {props.elementConfig.options.map(el => <option key={el.value} value={el.value}>{el.displayValue}</option>)}
       </select>
       break;
     default: 
-      inputEle = <input className={classes.InputElement} {...props.elementConfig} value={props.value} onChange={props.change}/>
+      inputEle = <input className={inputClasses.join(' ')} {...props.elementConfig} value={props.value} onChange={props.change}/>
 
   }
   return <div className={classes.Input}> 
